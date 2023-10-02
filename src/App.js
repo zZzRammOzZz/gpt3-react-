d6f868c79001118a2c25970a124071af3d174ef3
Rami Meraachly <meraachlym@gmail.com>
2023-10-02 17:31:41 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+import { Navbar, Brand, Cta } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
